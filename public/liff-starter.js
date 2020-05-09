66ed53edb859c6e13233bf58bdf86fb7acace7e9
tiennv <tiennv.study@gmail.com>
2020-05-09 10:29:25 +0700

    change nodejs false

--- public/liff-starter.js
+++ public/liff-starter.js
@@ -1,5 +1,5 @@
 window.onload = function() {
-    const useNodeJS = true;   // if you are not using a node server, set this value to false
+    const useNodeJS = false;   // if you are not using a node server, set this value to false
     const defaultLiffId = "1610223629-7qJxOY2k";   // change the default LIFF value if you are not using a node server
 
     // DO NOT CHANGE THIS
